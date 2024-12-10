611152a42b305f5481de60444b8462ec0c2a0525
jbaii <jiaxinbai830@gmail.com>
2024-12-10 08:09:48 -0800

    updates

--- matchpred_app.py
+++ matchpred_app.py
@@ -1,50 +1,30 @@
-import pickle
-# we are going to use the Flask micro web framework
-from flask import Flask, request, jsonify
-from joblib import dump, load
+from flask import Flask, request, render_template, jsonify
+from joblib import load
 import numpy as np
-from mysklearn.myclassifiers import *
+
 app = Flask(__name__)
 
 def load_model():
-    # unpickle header and tree in tree.p
-    model = load("RF_math.joblib")
-    return model
-# we need to add some routes!
-# a "route" is a function that handles a request
-# e.g. for the HTML content for a home page
-# or for the JSON response for a /predict API endpoint, etc
-@app.route("/")
+    # 加载预训练模型
+    return load("RF_math.joblib")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    # return content and status code
-    return "<h1>Welcome to the interview predictor app</h1>", 200
-        
-
-# lets add a route for the /predict endpoint
-@app.route("/predict")
-def predict():
-    # lets parse the unseen instance values from the query string
-    # they are in the request object
-    home_rank = request.args.get("home_team_rank") # defaults to None
-    away_rank = request.args.get("away_team_rank")
-    instance = [home_rank, away_rank]
-    model = load_model()
-    # lets make a prediction!
-    pred = model.predict([np.array([float(home_rank), float(away_rank)])])
-    if pred is not None:
-        return jsonify({"prediction": pred}), 200
-    # something went wrong!!
-    # print(pred)
-
-
-
+    prediction = None
+    if request.method == "POST":
+        try:
+            # 获取两个输入
+            home_team_rank = float(request.form.get("home_team_rank", 0))
+            away_team_rank = float(request.form.get("away_team_rank", 0))
+            
+            # 预测
+            model = load_model()
+            pred = model.predict([[home_team_rank, away_team_rank]])
+            prediction = pred[0]  # 假设模型返回的是一个列表
+        except Exception as e:
+            prediction = f"Error: {str(e)}"
+        print(prediction)
+    return render_template("./index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    # header, tree = load_model()
-    # print(header)
-    # print(tree)
-    app.run(host="0.0.0.0", port=5001, debug=False)
-    # TODO: when deploy app to "production", set debug=False
-    # and check host and port values
-
-    # instructions for deploying flask app to render.com: https://docs.render.com/deploy-flask
+    app.run(host="0.0.0.0", port=5001, debug=True)
